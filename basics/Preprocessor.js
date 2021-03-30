ad38d96854aa6c5bd843339d7545bdbc4da5fc08
soviet <45714679+sovlet@users.noreply.github.com>
2021-03-30 18:28:11 +0500

    1.1.7

--- basics/Preprocessor.js
+++ basics/Preprocessor.js
@@ -9,18 +9,22 @@ class Preprocessor {
     process(template) {
         var content = fs.readFileSync(this.file, 'utf8');
         var last = 0;
-        var fillers = Array.from(content.matchAll(/<\?js(.*?)\?>/gis));
-        this.parsed = [];
-        for (let i = 0; i < fillers.length; i++) {
-            if (fillers[i].index > 0) {
-                this.parsed.push(["plain", content.substring(last, fillers[i].index)]);
-            }
-            this.parsed.push(["js", new vm.Script(template.replace('<?code>', fillers[i][1]))]);
-            last = fillers[i].index + fillers[i][0].length;
-            if (i == fillers.length - 1) {
-                this.parsed.push(["plain", content.substring(last)]);
-            }
-        };
+        var fillers = Array.from(content.matchAll(/<\?(js)?(.*?)\?>/gis));
+        if(fillers.length == 0) {
+            this.parsed = [["plain", content]];
+        } else {
+            this.parsed = [];
+            for (let i = 0; i < fillers.length; i++) {
+                if (fillers[i].index > 0) {
+                    this.parsed.push(["plain", content.substring(last, fillers[i].index)]);
+                }
+                this.parsed.push(["js", new vm.Script(template.replace('<?code>', fillers[i][2]))]);
+                last = fillers[i].index + fillers[i][0].length;
+                if (i == fillers.length - 1) {
+                    this.parsed.push(["plain", content.substring(last)]);
+                }
+            };
+        }
         return this;
     }
     async use($, req, res) {
