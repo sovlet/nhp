4fac7134f718bdba0552837d152698f6c5e70722
sovlet <sovietxd@yandex.ru>
2021-07-14 15:00:59 +0500

    v2.0.1 fix bugs

--- src/nhp/index.js
+++ src/nhp/index.js
@@ -4,77 +4,93 @@
 //  E-mail:     soviet@s0viet.ru
 //  Web:        https://s0viet.ru/
 
-const path = require('path')
 const vm = require('vm')
+const fs = require('fs')
+const path = require('path')
 
-const template = require('$nhp/data/template')
+const template = require('@nhp/data/template')
 
 class NHP {
 	constructor(root) {
 		this.webMap = new Object(null)
-		let parsed = this.parseDir(root)
+		let full = path.join(process.cwd(), root)
+		let parsed = this.parseDir(full)
 		for(let dir of parsed) {
-			let rel = dir.substr(root.length)
-			switch(path.extname) {
+			let rel = dir.substr(full.length).replace(/\\\\*/g, '/')
+			switch(path.extname(dir)) {
 				case '.nhp':
-					if(webMap[rel] !== undefined)
+					if(this.webMap[rel] !== undefined)
 						continue
-					webMap[rel] = this.preprocess(dir)
+					this.webMap[rel] = this.preprocess(dir)
 					break
 				case '':
-					webMap[rel] = webMap[rel + '/'] = webMap[rel + '/index.php'] = webMap[rel + '/index.nhp'] || this.preprocess(dir + '/index.nhp')
+					this.webMap[rel] = this.webMap[rel + '/'] = this.webMap[rel + '/index.php'] = this.webMap[rel + '/index.nhp'] || this.preprocess(dir + '\\\\index.nhp') || this.webMap[rel + '/index.html']
 					break
 				default:
-					webMap[dir.substr(root.length)] = dir
+					this.webMap[rel] = dir
 			}
 		}
 	}
+	parseDir(dir) {
+		let array = [dir];
+		let elems = fs.readdirSync(dir, {withFileTypes: true});
+		for(let elem of elems) {
+			if(elem.isDirectory()) {
+				array = array.concat(this.parseDir(path.resolve(dir, elem.name)));
+				continue
+			}
+			array.push(path.resolve(dir, elem.name));
+		}
+		return array;
+	}
 	use(context) {
 		return async(req, res, next) => {
-			this.query(
+			if(false === await this.query(
 				vm.createContext({$: context, ... {'req': req, 'res': res}}),
 				req,
 				res
-			)
-			next()
+			)) next()
 		}
 	}
 	preprocess(file) {
-		let content = fs.readFileSync(this.file, 'utf8')
+		if(!fs.existsSync(file))
+			return
+		let content = fs.readFileSync(file, 'utf8')
 		let matches = Array.from(content.matchAll(/((<\?\j*s*)(.+)(\?>)|(.+))/gis))
-		let units = new Array(array.length)
+		let units = []
 		for(let match of matches) {
 			if(match[3])
-				units[units.length] = new vm.Script(template.replace('%0', match[3]))
+				units.push(new vm.Script(template.replace('%0', match[3])))
 			else
-				units[units.length] = match[5]
+				units.push(match[5])
 		}
-		return parsed
+		return units
 	}
-	process(context, units) {
+	async process(context, units) {
 		let content = ''
-        for(let unit of units) {
-            if(unit instanceof vm.Script) {
-            	try {
-            		content += (await unit.runInContext(context))
-            	} catch(error) {
-            		content += '<html><head></head><body><h1>Fatal error</h1><br><b>'
-            		content += error.stack.replace(/</gi, '&lt;').replace(/\n/gs, '<br>')
-            		content += '</b></body></html>'
-            	}
-            	continue
-            }
-            content += unit.toString()
-        }
-        return content
+		for(let unit of units) {
+			if(unit instanceof vm.Script) {
+				try {
+					await unit.runInContext(context)
+					content += context.__return__
+				} catch(error) {
+					content += '<html><head></head><body><h1>Fatal error</h1><br><b>'
+					content += error.stack.replace(/</gi, '&lt;').replace(/\n/gs, '<br>')
+					content += '</b></body></html>'
+				}
+				continue
+			}
+			content += unit.toString()
+		}
+		return content
 	}
-	query(context, req, res) {
+	async query(context, req, res) {
 		let data = this.webMap[req.path]
 		if(typeof data === "string")
 			return res.status(200).sendFile(data)
 		if(data instanceof Array)
-			return res.status(200).setHeader("Content-Type", "text/html;charset=utf-8").end(this.process(context, data))
-		return res.status(404).end();
+			return res.status(200).setHeader("Content-Type", "text/html;charset=utf-8").end(await this.process(context, data), 'utf8')
+		return false;
 	}
 }
 
