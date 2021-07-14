d330fdd24bce24908c8240f910628ed6813f976a
sovlet <sovietxd@yandex.ru>
2021-07-14 16:32:46 +0500

    added pkg

---
+++ src/pkg/index.js
@@ -0,0 +1,11 @@
+//  Module:     src/pkg
+//  Project:    nhp
+//  Author:     soviet
+//  E-mail:     soviet@s0viet.ru
+//  Web:        https://s0viet.ru/
+
+const packageJson = require('@nhp/package.json')
+
+module.exports = {
+	version: packageJson.version
+}
