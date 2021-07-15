c62b58d8b84173122333573612bae087c65ecd75
sovlet <sovietxd@yandex.ru>
2021-07-15 20:45:08 +0500

    new template

--- data/template/index.js
+++ data/template/index.js
@@ -4,4 +4,4 @@
 //  E-mail:     soviet@s0viet.ru
 //  Web:        https://s0viet.ru/
 
-module.exports = 'function echo(...r){for(s of r)__return__+=s}function exit(){throw new Error({})}__return__="",async function(){await async function(){try{%0;exit()}catch(r){r.message!={}&&(__return__="<html><head></head><body><h1>Internal error</h1><br><b>"+r.stack.replace(/</gi,"&lt;").replace(/\\n/gs,"<br>")+"</b></body></html>")}}(),__return__}();'
+module.exports = 'function echo(..._){for(s of _)__return__+=s}function exit(){throw new Error({})}function __main__(){%0}__return__="",async function(){try{__main__(),exit()}catch(_){_.message!={}&&(__return__="<html><head></head><body><h1>Internal error</h1><br><b>"+_.stack.replace(/</gi,"&lt;").replace(/\\n/gs,"<br>")+"</b></body></html>")}}(),__return__;'
